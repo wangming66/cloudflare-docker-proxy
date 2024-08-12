e19ef91bb60cd1ee4f9dcc7469c264353faa6244
lfmwuy <38776593+lfmwuy@users.noreply.github.com>
2024-08-12 15:48:40 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.lfmwuy.top": dockerHub,
+  "quay.lfmwuy.top": "https://quay.io",
+  "gcr.lfmwuy.top": "https://gcr.io",
+  "k8s-gcr.lfmwuy.top": "https://k8s.gcr.io",
+  "k8s.lfmwuy.top": "https://registry.k8s.io",
+  "ghcr.lfmwuy.top": "https://ghcr.io",
+  "cloudsmith.lfmwuy.top": "https://docker.cloudsmith.io",
+  "ecr.lfmwuy.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.lfmwuy.top": dockerHub,
 };
 
 function routeByHosts(host) {
